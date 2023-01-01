0ba7e08a9bbcede8ca25393e5e79ad33f7573fa8
preeti <pritich0896@gmail.com>
2023-01-01 16:07:28 +0530

    Calculate Length

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Day8_LineComparison;
+
+namespace Day8_LineCompaison
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LengthOfLine.line();
+
+            Console.ReadLine();
+        }
+    }
+}
